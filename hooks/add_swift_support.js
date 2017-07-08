938743e65a0b8b1bc786369859d8f4112cbe5874
Dave Alden <dpa99c@gmail.com>
2017-07-08 21:25:27 +0100

    Fix previous commit

--- hooks/add_swift_support.js
+++ hooks/add_swift_support.js
@@ -10,6 +10,14 @@ module.exports = function(context) {
 
     run();
 
+    function get_iosPlatformPath() {
+      return path.join(get_projectRoot(), 'platforms', 'ios');
+    }
+
+    function get_projectRoot() {
+      return context.opts.projectRoot;
+    }
+
     function run() {
         var cordova_util = context.requireCordovaModule('cordova-lib/src/cordova/util'),
             ConfigParser = CORDOVA_VERSION >= 6.0
@@ -31,16 +39,16 @@ module.exports = function(context) {
               try {
                 // try pre-5.0 cordova structure
                 platform_ios = context.requireCordovaModule('cordova-lib/src/plugman/platforms')['ios'];
-                projectFile = platform_ios.parseProjectFile(iosPlatformPath());
+                projectFile = platform_ios.parseProjectFile(get_iosPlatformPath());
               } catch (e) {
                 // let's try cordova 5.0 structure
                 try {
                   platform_ios = context.requireCordovaModule('cordova-lib/src/plugman/platforms/ios');
-                  projectFile = platform_ios.parseProjectFile(iosPlatformPath());
+                  projectFile = platform_ios.parseProjectFile(get_iosPlatformPath());
                 } catch(e) {
                   // try cordova 7.0 structure
-                  var iosPlatformApi = require(path.join(iosPlatformPath(), '/cordova/Api'));
-                  var projectFileApi = require(path.join(iosPlatformPath(), '/cordova/lib/projectFile.js'));
+                  var iosPlatformApi = require(path.join(get_iosPlatformPath(), '/cordova/Api'));
+                  var projectFileApi = require(path.join(get_iosPlatformPath(), '/cordova/lib/projectFile.js'));
                   var locations = (new iosPlatformApi()).locations;
                   projectFile = projectFileApi.parse(locations);
                 }
