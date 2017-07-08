373a31e84140941646854d30e7cab22d4d505127
Dave Alden <dpa99c@gmail.com>
2017-07-08 21:19:39 +0100

    Patch hook script to work with cordova@7

--- hooks/add_swift_support.js
+++ hooks/add_swift_support.js
@@ -20,9 +20,6 @@ module.exports = function(context) {
             xml = cordova_util.projectConfig(projectRoot),
             cfg = new ConfigParser(xml),
             projectName = cfg.name(),
-            platform_ios = CORDOVA_VERSION < 5.0
-              ? context.requireCordovaModule('cordova-lib/src/plugman/platforms')['ios']
-              : context.requireCordovaModule('cordova-lib/src/plugman/platforms/ios'),
             iosPlatformPath = path.join(projectRoot, 'platforms', 'ios'),
             iosProjectFilesPath = path.join(iosPlatformPath, projectName),
             xcconfigPath = path.join(iosPlatformPath, 'cordova', 'build.xcconfig'),
@@ -31,7 +28,24 @@ module.exports = function(context) {
             xcodeProject,
             bridgingHeaderPath;
 
-        projectFile = platform_ios.parseProjectFile(iosPlatformPath);
+              try {
+                // try pre-5.0 cordova structure
+                platform_ios = context.requireCordovaModule('cordova-lib/src/plugman/platforms')['ios'];
+                projectFile = platform_ios.parseProjectFile(iosPlatformPath());
+              } catch (e) {
+                // let's try cordova 5.0 structure
+                try {
+                  platform_ios = context.requireCordovaModule('cordova-lib/src/plugman/platforms/ios');
+                  projectFile = platform_ios.parseProjectFile(iosPlatformPath());
+                } catch(e) {
+                  // try cordova 7.0 structure
+                  var iosPlatformApi = require(path.join(iosPlatformPath(), '/cordova/Api'));
+                  var projectFileApi = require(path.join(iosPlatformPath(), '/cordova/lib/projectFile.js'));
+                  var locations = (new iosPlatformApi()).locations;
+                  projectFile = projectFileApi.parse(locations);
+                }
+             }
+
         xcodeProject = projectFile.xcode;
 
         if (fs.existsSync(xcconfigPath)) {
